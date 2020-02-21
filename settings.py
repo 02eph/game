e27de8c1468853dd52f9bbdd300212256157584d
02eph <nastya.volodenok@gmail.com>
2020-02-21 19:47:59 +0300

    chapter 12

--- settings.py
+++ settings.py
@@ -1,6 +1,12 @@
 class Settings():
     def __init__(self):
-        self.screen_width = 800
+        self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (147, 112, 219)
-        self.ship_speed_factor = 1.5
+        self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 2
+
+        self.bullet_speed_factor = 3.5
+        self.bullet_width = 4
+        self.bullet_height = 20
+        self.bullet_color = (255, 255, 255)
+        self.bullets_allowed = 5
